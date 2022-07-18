f5c409f5ab50f2f7c94fbb9b0998a51666fa617d
Krasimir Emilov <94074929+krasi11@users.noreply.github.com>
2022-07-18 18:52:06 +0300

    Create StartUp.cs

---
+++ Inheritance/RandomList/StartUp.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace CustomRandomList
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello!");
+        }
+    }
+}
