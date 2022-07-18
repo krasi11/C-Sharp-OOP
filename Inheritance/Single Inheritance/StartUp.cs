76edc7f12726d441ec1e72c0cfb7b7c95195748b
Krasimir Emilov <94074929+krasi11@users.noreply.github.com>
2022-07-18 18:24:48 +0300

    Create StartUp.cs

---
+++ Inheritance/Single Inheritance/StartUp.cs
@@ -0,0 +1,15 @@
+using System;
+
+namespace Farm
+{
+    class StartUp
+    {
+        static void Main(string[] args)
+        {
+            Dog dog = new Dog();
+            dog.barking();
+            dog.barking();
+            dog.Eat();
+        }
+    }
+}
