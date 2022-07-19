54e030d180eb2b660f7d411b44ad1971499291c9
Krasimir Emilov <94074929+krasi11@users.noreply.github.com>
2022-07-19 20:07:48 +0300

    Create Person.cs

---
+++ Encapsulation-Excercise/Shopping Spree/Person.cs
@@ -0,0 +1,74 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Encapsulation_Exc
+{
+    class Person
+    {
+        private string name;
+        private decimal money;
+        private List<Product> bagOfProducts;
+
+        public Person(string name, decimal money)
+        {
+            Name = name;
+            Money = money;
+            BagOfProducts = new List<Product>();
+        }
+
+        public List<Product> BagOfProducts
+        {
+            get { return bagOfProducts; }
+            private set { bagOfProducts = value; }
+        }
+
+
+        public decimal Money
+        {
+            get { return money; }
+            private set
+            {
+                if (value < 0)
+                {
+                    throw new ArgumentException("Money cannot be negative");
+                }
+                money = value;
+            }
+        }
+
+
+        public string Name
+        {
+            get { return name; }
+            private set
+            {
+                if (string.IsNullOrWhiteSpace(value))
+                {
+                    throw new ArgumentException("Name cannot be empty");
+                }
+                name = value;
+            }
+        }
+
+        public void Buy(Product product)
+        {
+            if (Money < product.Cost)
+            {
+                throw new InvalidOperationException($"{Name} can't afford {product.Name}");
+
+            }
+
+            Money -= product.Cost;
+            BagOfProducts.Add(product);
+            Console.WriteLine($"{Name} bought {product.Name}");
+
+
+        }
+
+        public override string ToString()
+            => $"{Name} - {(BagOfProducts.Any() ? string.Join(", ", BagOfProducts) : "Nothing bought")}";
+    }
+}
