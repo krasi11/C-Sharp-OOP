3b432cb643d020452f9364dec9e88b05ff5b9d4a
Krasimir Emilov <94074929+krasi11@users.noreply.github.com>
2022-07-21 19:38:07 +0300

    Create Gorilla.cs

---
+++ Inheritance-Exercice/Zoo/Gorilla.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Inheritance_excc
+{
+    class Gorilla:Mammal
+    {
+        public Gorilla(string name) : base(name)
+        {
+
+        }
+    }
+}
