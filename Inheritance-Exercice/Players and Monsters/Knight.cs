fd5888ca21b4d579da74143091d54ecd0479e477
Krasimir Emilov <94074929+krasi11@users.noreply.github.com>
2022-07-21 20:15:44 +0300

    Create Knight.cs

---
+++ Inheritance-Exercice/Players and Monsters/Knight.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Inheritance_excc
+{
+    public class Knight:Hero
+    {
+        public Knight(string username,int level):base(username,level)
+        {
+
+        }
+    }
+}
