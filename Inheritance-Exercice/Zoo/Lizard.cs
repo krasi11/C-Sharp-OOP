84ca2b47a40c4ffc070349c2144034b6ac8aa09a
Krasimir Emilov <94074929+krasi11@users.noreply.github.com>
2022-07-21 19:39:34 +0300

    Create Lizard.cs

---
+++ Inheritance-Exercice/Zoo/Lizard.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Inheritance_excc
+{
+    class Lizard:Reptile
+    {
+        public Lizard(string name) : base(name)
+        {
+
+        }
+    }
+}
