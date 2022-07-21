c5250384f54e6b84a7a648ec6ad0e49bb3646519
Krasimir Emilov <94074929+krasi11@users.noreply.github.com>
2022-07-21 21:32:47 +0300

    Create BladeKnight.cs

---
+++ Inheritance-Exercice/Players and Monsters/BladeKnight.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Inheritance_excc
+{
+   public class BladeKnight:Knight
+    {
+        public BladeKnight(string username,int level) : base(username, level)
+        {
+
+        }
+    }
+}
