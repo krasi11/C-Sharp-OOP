09d2ba9b037f2c55be6281f29f06b1dd759a3e1f
Krasimir Emilov <94074929+krasi11@users.noreply.github.com>
2022-07-21 19:37:28 +0300

    Create Bear.cs

---
+++ Inheritance-Exercice/Zoo/Bear.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Inheritance_excc
+{
+    class Bear:Mammal
+    {
+        public Bear(string name) : base(name)
+        {
+
+        }
+    }
+}
