69692112be2ccf2e31f4c79691cc56f873ee5ab7
Krasimir Emilov <94074929+krasi11@users.noreply.github.com>
2022-07-21 20:15:23 +0300

    Create Wizard.cs

---
+++ Inheritance-Exercice/Players and Monsters/Wizard.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Inheritance_excc
+{
+    public class Wizard:Hero
+    {
+        public Wizard(string username,int level) : base(username, level)
+        {
+
+        }
+    }
+}
