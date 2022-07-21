88d84558c6f2ff6bbf3a1269025875e79ff794ac
Krasimir Emilov <94074929+krasi11@users.noreply.github.com>
2022-07-21 20:16:45 +0300

    Create Elf.cs

---
+++ Inheritance-Exercice/Players and Monsters/Elf.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Inheritance_excc
+{
+    public class Elf:Hero
+    {
+        public Elf(string username, int level) : base(username, level)
+        {
+
+        }
+    }
+}
