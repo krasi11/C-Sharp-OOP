2eeca7d5165e08fd70b4d9ad25440b35bb406be4
Krasimir Emilov <94074929+krasi11@users.noreply.github.com>
2022-07-21 19:36:30 +0300

    Create Mammal.cs

---
+++ Inheritance-Exercice/Zoo/Mammal.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Inheritance_excc
+{
+    class Mammal:Animal
+    {
+        public Mammal(string name) : base(name)
+        {
+
+        }
+    }
+}
