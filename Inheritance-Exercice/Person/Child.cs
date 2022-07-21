a6146d40baf9c29be349f8328c69d98bc32c3ede
Krasimir Emilov <94074929+krasi11@users.noreply.github.com>
2022-07-21 18:45:20 +0300

    Create Child.cs

---
+++ Inheritance-Exercice/Person/Child.cs
@@ -0,0 +1,11 @@
+namespace Person
+{
+    public class Child : Person
+    {
+        public Child(string name, int age) 
+            : base(name, age)
+        {
+            
+        }
+    }
+}
