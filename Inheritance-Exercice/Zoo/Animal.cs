07f232c43d9cd6748a4098255d2a8faf98f10a20
Krasimir Emilov <94074929+krasi11@users.noreply.github.com>
2022-07-21 19:35:32 +0300

    Create Animal.cs

---
+++ Inheritance-Exercice/Zoo/Animal.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Inheritance_excc
+{
+    class Animal
+    {
+        public Animal (string name)
+        {
+            Name = name;
+        }
+        public string Name { get; set; }
+    }
+}
