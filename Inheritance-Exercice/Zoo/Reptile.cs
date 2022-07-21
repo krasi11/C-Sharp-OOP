3003c4cdd843203652e9a65e34a51286978934f9
Krasimir Emilov <94074929+krasi11@users.noreply.github.com>
2022-07-21 19:38:54 +0300

    Create Reptile.cs

---
+++ Inheritance-Exercice/Zoo/Reptile.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Inheritance_excc
+{
+    class Reptile:Animal
+    {
+        public Reptile(string name) : base(name)
+        {
+
+        }
+    }
+}
