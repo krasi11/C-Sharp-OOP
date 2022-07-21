2266e444c23127b8b54bb70dc2b30115732de3ef
Krasimir Emilov <94074929+krasi11@users.noreply.github.com>
2022-07-21 22:04:30 +0300

    Create Vehicle.cs

---
+++ Inheritance-Exercice/Need For Speed/Vehicle.cs
@@ -0,0 +1,23 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NeedForSpeed
+{
+    public  class Vehicle
+    {
+        public Vehicle(int horsePower,double fuel)
+        {
+
+        }
+        public double Fuel { get; set; }
+        public int HorsePower { get; set; }
+        public double DefaultFuelConsumption { get; set; }
+        public virtual double FuelConsumption { get; set; }
+
+        public virtual void Drive (double kilometers)
+        {
+            Fuel -= DefaultFuelConsumption * kilometers;
+        }
+    }
+}
