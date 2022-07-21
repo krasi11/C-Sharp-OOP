b0d27504a5ccbb67c0b94e5b60ab71e90c2a02cb
Krasimir Emilov <94074929+krasi11@users.noreply.github.com>
2022-07-21 19:51:31 +0300

    Create StartUp.cs

---
+++ Inheritance-Exercice/Zoo/StartUp.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace Inheritance_excc
+{
+    class StartUp
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
