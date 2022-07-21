c9e83d8fe36cb435ddb9861a82b2caf00006192d
Krasimir Emilov <94074929+krasi11@users.noreply.github.com>
2022-07-21 21:33:10 +0300

    Create DarkKnight.cs

---
+++ Inheritance-Exercice/Players and Monsters/DarkKnight.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Inheritance_excc
+{
+    public class DarkKnight:Knight
+    {
+        public DarkKnight(string username,int level):base(username,level)
+        {
+
+        }
+    }
+}
