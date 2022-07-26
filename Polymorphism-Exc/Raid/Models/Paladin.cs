6795ec2cf1c96ddc24d0a7964253aecb7a290107
Krasimir Emilov <94074929+krasi11@users.noreply.github.com>
2022-07-26 18:28:39 +0300

    Create Paladin.cs

---
+++ Polymorphism-Exc/Raid/Models/Paladin.cs
@@ -0,0 +1,19 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Raiding 
+{
+    public class Paladin : BaseHero
+    {
+        private const int DefaultPower = 100;
+
+        public Paladin(string name) : base(name, DefaultPower)
+        {
+
+        }
+        public override string CastAbility() => base.CastAbility() + $" healed for {Power}";
+    }
+}
