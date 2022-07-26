e9f2608cf7471b870f0898bfe8af556dfe7e3868
Krasimir Emilov <94074929+krasi11@users.noreply.github.com>
2022-07-26 18:28:16 +0300

    Create BaseHero.cs

---
+++ Polymorphism-Exc/Raid/Models/BaseHero.cs
@@ -0,0 +1,21 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Raiding
+{
+    public abstract class BaseHero
+    {
+        protected  BaseHero(string name,int power)
+        {
+            Name = name;
+            Power = power;
+        }
+        public string Name { get; set; }
+        public int Power { get; set; }
+
+        public virtual string CastAbility() => $"{GetType().Name} - {Name}";
+    }
+}
