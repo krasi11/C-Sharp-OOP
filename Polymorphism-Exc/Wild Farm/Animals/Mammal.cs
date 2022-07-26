17c54cba371877783d9b2e8ed51650aec019f658
Krasimir Emilov <94074929+krasi11@users.noreply.github.com>
2022-07-26 18:47:21 +0300

    Create Mammal.cs

---
+++ Polymorphism-Exc/Wild Farm/Animals/Mammal.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WildFarm.Animals
+{
+    public abstract class Mammal:Animal
+    {
+        protected Mammal(string name,double weight,string livingRegion) : base(name, weight)
+        {
+            LivingRegion = livingRegion;
+        }
+
+        public string LivingRegion { get; set; }
+    }
+}
