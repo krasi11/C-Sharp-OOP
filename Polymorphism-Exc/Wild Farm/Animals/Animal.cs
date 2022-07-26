1bfe995ab631bcfb9b670316c19415abe3acf18a
Krasimir Emilov <94074929+krasi11@users.noreply.github.com>
2022-07-26 19:23:21 +0300

    Create Animal.cs

---
+++ Polymorphism-Exc/Wild Farm/Animals/Animal.cs
@@ -0,0 +1,29 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WildFarm
+{
+    public abstract class Animal
+    {
+        public Animal(string name, double weight)
+        {
+            Name = name;
+            Weight = weight;
+        }
+
+        public abstract double WeightGainPerUnitOfFood { get; }
+        public string Name { get; protected set; }
+        public double Weight { get; protected set; }
+        public int FoodEaten { get; protected set; } = 0;
+
+        public virtual void Feed(Food food)
+        {
+            Weight += WeightGainPerUnitOfFood * food.Quantity;
+            FoodEaten += food.Quantity;
+        }
+        public abstract string ProduceSound();
+    }
+}
