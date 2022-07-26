2238b2b9fca115482a03f0066066feba93a18191
Krasimir Emilov <94074929+krasi11@users.noreply.github.com>
2022-07-26 18:51:41 +0300

    Create Feline.cs

---
+++ Polymorphism-Exc/Wild Farm/Animals/Feline.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WildFarm
+{
+   public abstract class Feline:Mammal
+    {
+        protected Feline(string name,double weight,string livingRegion,string breed) : base(name, weight,livingRegion)
+        {
+            Breed = breed;
+        }
+        public string Breed { get; set; }
+        public override string ToString() => $"{GetType().Name} [{Name}, {Breed}, {Weight}, {LivingRegion}, {FoodEaten}";
+    }
+}
