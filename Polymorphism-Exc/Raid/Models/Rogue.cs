9d27fc959ab875ae10c1436ec79ef64a7185cc5b
Krasimir Emilov <94074929+krasi11@users.noreply.github.com>
2022-07-26 18:28:49 +0300

    Create Rogue.cs

---
+++ Polymorphism-Exc/Raid/Models/Rogue.cs
@@ -0,0 +1,19 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Raiding
+{
+    public class Rogue : BaseHero
+    {
+        private const int DefaultPower = 80;
+
+        public Rogue(string name) : base(name, DefaultPower)
+        {
+
+        }
+        public override string CastAbility() => base.CastAbility() + $" hit for {Power} damage";
+    }
+}
