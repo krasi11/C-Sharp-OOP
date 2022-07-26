893c3732ecda392d74cb85385c692ba3c70eac73
Krasimir Emilov <94074929+krasi11@users.noreply.github.com>
2022-07-26 19:27:03 +0300

    Create StartUp.cs

---
+++ Polymorphism-Exc/Wild Farm/StartUp.cs
@@ -0,0 +1,43 @@
+using System;
+using System.Collections.Generic;
+
+namespace WildFarm
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            List<Animal> animals = new List<Animal>();
+
+            string input = Console.ReadLine();
+
+            while (input != "End")
+            {
+                string[] animalData = input.Split(' ', StringSplitOptions.RemoveEmptyEntries);
+                Animal animal = AnimalFactory.CreateAnimal(animalData);
+
+                string[] foodData = Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries);
+                Food.Food food = FoodFactory.CreateFood(foodData[0], int.Parse(foodData[1]));
+
+                Console.WriteLine(animal.ProduceSound());
+
+                try
+                {
+                    animal.Feed(food);
+                }
+                catch (Exception exception)
+                {
+                    Console.WriteLine(exception.Message);
+                }
+
+                animals.Add(animal);
+                input = Console.ReadLine();
+            }
+
+            foreach (var animal in animals)
+            {
+                Console.WriteLine(animal);
+            }
+        }
+    }
+}
