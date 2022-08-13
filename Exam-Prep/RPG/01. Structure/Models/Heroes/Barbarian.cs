b7de2a7a809e91f17ce6df8b1d484ba1ecfd1414
Krasimir Emilov <94074929+krasi11@users.noreply.github.com>
2022-08-13 15:00:31 +0300

    Create Barbarian.cs

---
+++ Exam-Prep/RPG/01. Structure/Models/Heroes/Barbarian.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Heroes.Models.Contracts;
+
+namespace Heroes.Models.Heroes
+{
+    public class Barbarian : Hero
+    {
+        public Barbarian(string name, int health, int armour) : base(name, health, armour)
+        {
+        }
+    }
+}
