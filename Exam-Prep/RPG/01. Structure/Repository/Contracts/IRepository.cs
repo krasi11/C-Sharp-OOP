c452fe8afd1938238326383839e05029c88b1c5d
Krasimir Emilov <94074929+krasi11@users.noreply.github.com>
2022-08-13 15:04:12 +0300

    Create IRepository.cs

---
+++ Exam-Prep/RPG/01. Structure/Repository/Contracts/IRepository.cs
@@ -0,0 +1,14 @@
+namespace Heroes.Repositories.Contracts
+{
+    using System.Collections.Generic;
+    public interface IRepository<T>
+    {
+        IReadOnlyCollection<T> Models { get; }
+
+        void Add(T model);
+
+        bool Remove(T model);
+
+        T FindByName(string name);
+    }
+}
